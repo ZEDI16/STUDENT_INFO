17026679da1efe90aeef67622d72c50f7969d2f0
ZEDI16 <108366444+ZEDI16@users.noreply.github.com>
2022-10-12 00:18:34 +0800

    Replaced.

--- Info_Castillo.java
+++ Info_Castillo.java
@@ -1,34 +1,34 @@
 package src;
 
 import java.util.Scanner;
-
 public class Info_Castillo {
-
 	static Scanner castillo_Scan = new Scanner(System.in);
 	   
 	    private static String LastName,FirstName,MiddleName,Section = null,Gradelvl;
-	    private static int Birth, year = 2022,Birth2,Birth3,grade1 = 0,grade2 = 0,grade3 = 0,grade4 = 0;
-	    private static int Age,dob;
-		
-	public static void Main(){
-		System.out.print("WELCOME TO MY FINAL PROJ THIS SEMESTER\nCODER:Riceo G. Castillo");
-		System.out.println(" ");
+	    private static int Birth, year = 2022,Birth2,Birth3,grade1 = 0,grade2 = 0,grade3 = 0,grade4 = 0,Age;
+	    public static void Main(){
+	        System.out.println("\t\t\t\t+-----------------------------+\t\t\t\t");
+			System.out.println("\t\t\t\t+                             +\t\t\t\t");
+			System.out.println("\t\t\t\t+ WELCOME TO MY FINAL PROJECT +\t\t\t\t");
+			System.out.println("\t\t\t\t+ THIS SEMESTER.              +\t\t\t\t");
+			System.out.println("\t\t\t\t+                             +\t\t\t\t");
+			System.out.println("\t\t\t\t+ CODER: RICEO G> CASTILLO    +\t\t\t\t");
+			System.out.println("\t\t\t\t+-----------------------------+\t\t\t\t");
 	}
 	public static void NAMES(){
 		
 		System.out.println(" ");
 		System.out.print("DO INPUT YOUR LASTNAME:");
-		LastName = castillo_Scan.next();
+		LastName = castillo_Scan.nextLine();
 
 		System.out.print("DO INPUT YOUR FIRSTNAME:");
-		FirstName = castillo_Scan.next();
+		FirstName = castillo_Scan.nextLine();
 		
 		System.out.print("DO INPUT YOUR MIDDLENAME:");
-		MiddleName = castillo_Scan.next();
+		MiddleName = castillo_Scan.nextLine();
 	}
 
 	public static void AGE (){	
-	    
 		System.out.println(" ");
 		System.out.println("DO INPUT YOUR DATE OF BIRTH");
 		System.out.println(" ");
@@ -37,7 +37,6 @@ public class Info_Castillo {
 		Birth2 = castillo_Scan.nextInt();
 		Birth3 = castillo_Scan.nextInt();
 		Age = year - Birth3;
-
 	   }
 	public static void GRADEnSECTION(){	
 		System.out.println(" ");
@@ -62,7 +61,7 @@ public class Info_Castillo {
 		float DataOne = (grade1 + grade2 + grade3 + grade4);
 		float DataTwo = DataOne /4;
 		 String initials = MiddleName.substring(0,1);
-	
+		
 		System.out.println(" ");
 		System.out.println("=======");
 		System.out.println("OUTPUT:");
@@ -72,6 +71,7 @@ public class Info_Castillo {
 		System.out.print("\nSTUDENT:"+FirstName+" "+initials+"."+" "+LastName);
 		System.out.print("\nAGE:" + Age);
 		System.out.println("\nGRADE LEVEL & SECTION:" + Gradelvl+"-"+Section);
+		
 		System.out.print(" ");
 		System.out.print("\n1ST:" + grade1);
 		System.out.print("\n2ND:" + grade2);
@@ -82,30 +82,28 @@ public class Info_Castillo {
 		System.out.print(" ");
 	    System.out.print("\nREMARKS:");
 		if ( DataTwo >= 75) {
-			System.out.print("\nPASSED\n");
+			System.out.print("PASSED\n");
 		}
-		if ( DataTwo >= 90) {
-			System.out.print("\nHIGHEST");
+		if ( DataTwo >= 95) {
+			System.out.print("HIGH");
 		}
-    	if ( DataTwo >= 80) {
-			System.out.print(" HONOR");
+		if ( DataTwo >= 98) {
+			System.out.print("EST" +" ");
+		}
+    	if ( DataTwo >= 90) {
+			System.out.print(" "+"HONOR");
     	}
 		if ( DataTwo < 75){
-			System.out.print("\nFAILED\nRICEO YOU FAILED!");
+			System.out.print("FAILED\nRICEO YOU FAILED!");
 		}
-	  
-		
-		
+		System.out.print("\n[Program Finished]");
 	}
-	
 		public static void main(String[] args) {
 		Main();	
 		NAMES();
 		AGE();
 		GRADEnSECTION();
 		GRADEnRESULT();
-		
 		OUTPUTS();
 	}
-
 }
